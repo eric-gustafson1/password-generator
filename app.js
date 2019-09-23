c66d61ce71bc1f935be5fc7c5ddb27ce8dee3108
Eric Gustafson <weuxfya@gmail.com>
2019-09-23 15:37:03 -0600

    Add logic to provide feedback on password strength and clean up DOM

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// Variables for the 4 different character sets
 const special = '!@#$%^&*()'
 const numeric = '1234567890'
 const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const lowercase = 'abcdefghijklmnopqrstuvwxyz'
 
+// Variables for DOM elements
 let bootstrapSuccess = document.querySelector('.alert-success')
 let bootstrapWarning = document.querySelector('.alert-warning')
 let pwdResult = document.getElementById('password')
@@ -15,25 +17,27 @@ let generateBtn = document.getElementById('generate')
 let copyBtn = document.getElementById('copy')
 let passwordText = document.getElementById('password')
 let checkboxes = document.querySelectorAll('.form-check-input')
-let workingList = ''
+let rangeSlider = document.querySelector('.range')
+let rangeDlispay = document.getElementById('range')
+let rangeEval = document.querySelector('.range-eval')
+let card = document.querySelector('.card')
 
+// Variables for output
+let workingList = ''
 let resultArr = []
+let checkCount = 0
 
 // Hide Bootsrap 4 alerts on load
 bootstrapSuccess.style.display = 'none'
 bootstrapWarning.style.display = 'none'
 
-// console.log(checkboxes)
-
-
-
-// Generate a random character from list input parameter
+// Function to generate a random character from list input parameter
 function getRandomChar(list) {
     resultArr.push(list[Math.floor(Math.random() * list.length)])
 
 }
 
-// Copy password to clipboard function
+// Function to copy password to clipboard 
 function copyPassword() {
     let copyText = ''
     copyText = passwordText
@@ -43,9 +47,7 @@ function copyPassword() {
     bootstrapSuccess.style.display = 'block'
 }
 
-
-
-// Generate password function
+// Function to generate password based on input
 
 function generate() {
     // Hide both bootstrap alerts
@@ -55,41 +57,39 @@ function generate() {
     // Reset the result array to empty
     resultArr = []
 
-    // Create working list of characters based on User Criteria UI checkboxes
+    // Clear the working list of characters 
     workingList = ''
 
     // set loop count from Range UI
     let loopCount = pwdLength.value
-    
 
-    // convert the nodelist to an array to parse with forEach
-    checkboxes = Array.from(checkboxes)
+    // // Convert the nodelist to an array to parse with forEach
+    // checkboxes = Array.from(checkboxes)
 
-    // look at each checkbox and add char set to working list if checked
-    // add one char from each selected char set to make sure at least one char is present
+    // Look at each checkbox and add char set to working list if checked
     checkboxes.forEach(function (checkbox) {
 
         if (checkbox.checked) {
 
-            // if the special box is checked add special char set to workingList and add one special char to the reultArr.  Decrment loopCount
+            // if the special box is checked add the special char set to workingList and add one special char to the reultArr.  Decrment loopCount
             if (checkbox.id === 'specialBox') {
                 workingList += special
                 getRandomChar(special)
                 loopCount--
 
-            // if the numeric box is checked add numeric char set to workingList and add one numeric char to the reultArr. Decrement loopcount
+                // if the numeric box is checked add the numeric char set to workingList and add one numeric char to the reultArr. Decrement loopcount
             } else if (checkbox.id === 'numericBox') {
                 workingList += numeric
                 getRandomChar(numeric)
                 loopCount--
 
-            // if the uppercase box is checked add uppercase char set to workingList and add one uppercase char to the reultArr. Decrement loopcount
+                // if the uppercase box is checked add the uppercase char set to workingList and add one uppercase char to the reultArr. Decrement loopcount
             } else if (checkbox.id === 'uppercaseBox') {
                 workingList += uppercase
                 getRandomChar(uppercase)
                 loopCount--
 
-            // if the lowercase box is checked add lowercase char set to workingList and add one lowercase char to the reultArr. Decrement loopcount
+                // if the lowercase box is checked add the lowercase char set to workingList and add one lowercase char to the reultArr. Decrement loopcount
             } else if (checkbox.id === 'lowercaseBox') {
                 workingList += lowercase
                 getRandomChar(lowercase)
@@ -121,8 +121,7 @@ function generate() {
 
     // Randomize/shuffle the array
     // Using Fisher-Yates Algorithm (https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb)
-
-    for(let j = resultArr.length - 1; j > 0; j--) {
+    for (let j = resultArr.length - 1; j > 0; j--) {
         const k = Math.floor(Math.random() * j)
         const temp = resultArr[j]
         resultArr[j] = resultArr[k]
@@ -134,7 +133,40 @@ function generate() {
 
     // Update the DOM
     pwdResult.textContent = resultArr
+}
 
+// Function to give UI feedback on password length
+function passwordFeedback() {
+
+    // Convert the nodelist to an array to parse with forEach
+    checkboxes = Array.from(checkboxes)
+    checkCount = 0
+
+    // Count the number of checked char set boxes
+    checkboxes.forEach(function (checkbox) {
+        if (checkbox.checked) {
+            checkCount++
+
+        }
+    })
+
+    if (pwdLength.value < 24 && checkCount < 2) {
+        rangeEval.textContent = 'Weak Password'
+        rangeEval.style.color = 'red'
+
+    } else if (pwdLength.value > 64 && checkCount > 2 && checkCount > 2) {
+        rangeEval.textContent = 'Strong Password'
+        rangeEval.style.color = 'green'
+
+    } else if (pwdLength.value > 24 || pwdLength.value < 64 && checkCount > 2) {
+        rangeEval.textContent = 'Medium Strength Password'
+        rangeEval.style.color = 'orange'
+
+    } else {
+        rangeEval.textContent = 'Medium Strength Password'
+        rangeEval.style.color = 'orange'
+
+    }
 
 }
 
@@ -144,3 +176,9 @@ generateBtn.addEventListener('click', generate)
 
 // Copy Password button Click
 copyBtn.addEventListener('click', copyPassword)
+
+// Card click events
+card.addEventListener('click', passwordFeedback)
+
+// Password length feedback
+// rangeSlider.addEventListener('click', passwordFeedback)
